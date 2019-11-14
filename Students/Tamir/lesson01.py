d35be2ed06ba2b66e6c6da812ce0c6347abf19f8
unknown <asta3000@gmail.com>
2019-11-14 21:41:02 +0800

    next commit

---
+++ Students/Tamir/lesson01.py
@@ -0,0 +1,30 @@
+print("Hello Tamir")
+
+
+a = 1
+print(type(a))
+
+
+x = 4
+y = 7
+print(x / y)
+print(x + y)
+print(x - y)
+print(x * y)
+print(str(x))
+print(float(y))
+
+
+print(pow(x , y))
+print(x ** y)
+print(y // x)
+
+print(x < y)
+print(x == y)
+print(x > y)
+print(x != y)
+
+x = 'abc'
+y = 'abc'
+
+print(x == y)
