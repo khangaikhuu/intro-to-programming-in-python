640f966b38d718e8349675b9684d09de5b88ddfd
khangaikhuu <khangaikhuu@googlemail.com>
2019-11-14 20:57:25 +0800

    next commit

---
+++ Students/Khangaikhuu/lesson01.py
@@ -0,0 +1,15 @@
+# Java , C#, C, C++ -- compiled
+
+# compiled 0/1 
+# 1. machine code/ binary code
+# 2. exe file binary 
+# Compiler
+
+# interpeted 
+# Python, Javascript, Ruby, PHP etc...
+# Idle
+# Interpreter
+
+# Environment variable 
+# PATH = C:/Programs/Python/3.7/bin
+
