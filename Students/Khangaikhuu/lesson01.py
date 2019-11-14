6f971e9090da9c25a99ee54eaaac90345d0e10ab
khangaikhuu <khangaikhuu@googlemail.com>
2019-11-14 21:41:16 +0800

    lesson 01

--- Students/Khangaikhuu/lesson01.py
+++ Students/Khangaikhuu/lesson01.py
@@ -11,5 +11,17 @@
 # Interpreter
 
 # Environment variable 
-# PATH = C:/Programs/Python/3.7/bin
+# PATH = ............;C:/Programs/Python/3.7/bin
+print("Hello Python")
 
+# Primitive type 
+# built in types
+# 1, 2, 2, 4, 4, - integer 
+# 1.4, 1.5, 2.7 - float - precision number
+# True, False - boolean - conditional
+# "Hello World" - string
+# Mainframes
+
+# variables example : x = 5, y = 4
+# Mathematical operations : 
+# comparisons
