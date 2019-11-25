d3b9c2f2cf0f731f85c231822d8ea094db6f2ab9
Muji <muji.sgs@gmail.com>
2019-11-25 19:46:35 +0800

    Assignment lesson02

---
+++ Students/Muji/lesson04.py
@@ -0,0 +1,4 @@
+my_dict={'first_name': 'Munkh', 'Last_name': 'Gankh'}
+river={'nile': 'Egypt', 'Eg': "Mongolia", 'Amazon': 'Brazil' }
+for item, value in river.items():
+    print(f" The {item.title()} runs through {value.item()}")
