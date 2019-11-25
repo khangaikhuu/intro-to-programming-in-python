1eed21e2ff67a8f8f3e4cb754b2cf222af6314c5
unknown <asta3000@gmail.com>
2019-11-25 17:42:29 +0800

    Lesson04

--- Students/Tamir/lesson04.py
+++ Students/Tamir/lesson04.py
@@ -1,5 +1,5 @@
-##Task 4.3
-print("Task 4.3\n")
+##Task 4.1
+print("Task 4.1\n")
 my_friend = {}
 my_friend['first_name'] = 'Tamir'
 my_friend['last_name'] = 'Bold'
@@ -11,8 +11,21 @@ print(my_friend['last_name'])
 print(my_friend['age'])
 print(my_friend['city_name'])
 
-##Task 4.3
-print("\nTask 4.4\n")
+
+##Task 4.2
+print("\nTask4.2")
+fav_number = {}
+for i in range(1, 6):
+    name = input("Name: ")
+    number = input("Number: ")
+    fav_number[name] = number
+
+for i, j in fav_number.items():
+    print(f"{i}'s favorite number is {j}.")
+
+
+##Task 4.4
+print("\nTask 4.4")
 famous_rivers= {
     'bold' : 'father',
     'enerel' : 'mother',
