4649e8a1b141bb5476e4b2248bcb4e31c8c52610
khangaikhuu <khangaikhuu@googlemail.com>
2019-12-07 21:26:58 +0800

    ignoring some files

--- Students/Khangaikhuu/lesson_09/ll_env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/khangaikhuu/Desktop/intro-to-programming-in-python/Students/Khangaikhuu/lesson_09/ll_env/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
