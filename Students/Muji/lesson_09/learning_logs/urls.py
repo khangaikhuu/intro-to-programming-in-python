008db046bd0c93354ddd4e3ed901572b38cd41b8
Muji <muji.sgs@gmail.com>
2019-12-09 21:39:35 +0800

    adding lesson10

--- Students/Muji/lesson_09/learning_logs/urls.py
+++ Students/Muji/lesson_09/learning_logs/urls.py
@@ -4,4 +4,8 @@ app_name = 'learning_logs'
 urlpatterns = [
     # Home page
     path('', views.index, name='index'),
+    path('topics/', views.topics, name='topics'),
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
+
+
 ]
