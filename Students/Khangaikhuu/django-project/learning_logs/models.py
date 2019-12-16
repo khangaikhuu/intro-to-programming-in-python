109da2a68e2ae187dcb3e51815d09984b0f2dd40
khangaikhuu <khangaikhuu@googlemail.com>
2019-12-17 05:36:00 +0800

    adding lesson 11

--- Students/Khangaikhuu/django-project/learning_logs/models.py
+++ Students/Khangaikhuu/django-project/learning_logs/models.py
@@ -2,6 +2,13 @@ from django.db import models
 
 # Create your models here.
 
+class Student(models.Model):
+    sure_name = models.CharField(max_length=350)
+    name = models.CharField(max_length=350)
+    course_name = models.CharField(max_length=350)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+
 class Topic(models.Model):
     """A topic the user is learning about."""
     text = models.CharField(max_length=200)
--- Students/Khangaikhuu/django-project/learning_logs/urls.py
+++ Students/Khangaikhuu/django-project/learning_logs/urls.py
@@ -10,5 +10,11 @@ urlpatterns = [
     path('topics/<int:topic_id>/', views.topic, name='topic'),
     # topics/1/ - Path parameter
     # topics?id=1 - Query parameter
+    # Page for adding a new topic
+    path('new_topic/', views.new_topic, name='new_topic'),
+    # Page for adding a new entry
+    path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),
+    # Page for editing an entry.
+    path('edit_entry/<int:entry_id>/', views.edit_entry, name='edit_entry'),
 ]
 
